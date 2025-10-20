20d5d6b228eaac06fb23c5fd3849776962e10688
berkahhariini999-rgb <berkahhariini999@gmail.com>
2025-10-20 13:36:08 +0700

    Initial Commit

---
+++ MyLittleJournal/MyLittleJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLittleJournalApp.swift
+//  MyLittleJournal
+//
+//  Created by Iqbal Alhadad on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MyLittleJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
